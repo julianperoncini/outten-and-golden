a70f6edc6a09d288c62436d84243a3b6a69a664d
Julian Peroncini <julianperoncini@gmail.com>
2025-05-04 20:42:55 -0300

    layout components

--- wp-content/themes/outten-and-golden/templates/text-template.php
+++ wp-content/themes/outten-and-golden/templates/text-template.php
@@ -1,11 +1,16 @@
 <?php
 /**
-    * Template Name: Text Template
-*/
+ * Template Name: Text Template
+ */
 
 use Timber\Timber;
 
 $context = Timber::context();
+$post = Timber::get_post();
+$context['post'] = $post;
+
+$context['item'] = get_fields();
+
 $template = 'views/pages/text-template/_index.twig';
 
 Timber::render($template, $context);
