62aff824a316d4cedaa5d3628ad71a700b1c5dd1
Wezz Òu <jesper@atronstore.com>
2025-05-04 04:33:33 +0200

    update

--- wp-content/themes/outten-and-golden/page.php
+++ wp-content/themes/outten-and-golden/page.php
@@ -16,4 +16,23 @@ $context = Timber::context();
 $context['post'] = Timber::get_post();
 $context['content_builder'] = get_fields($context['post']->ID)['content_builder'] ?? null;
 
+// Get issues posts
+$issues_posts = Timber::get_posts([
+     'post_type' => 'issues',
+     'posts_per_page' => -1,
+     'orderby' => 'date',
+     'order' => 'DESC',
+ ]);
+ 
+ // Get ACF fields for each issue
+ $issues_with_fields = [];
+ foreach ($issues_posts as $issue) {
+     $issue_fields = get_fields($issue->ID);
+     
+     $issue->fields = $issue_fields;
+     $issues_with_fields[] = $issue;
+ }
+ 
+ $context['issues'] = $issues_with_fields;
+
 Timber::render( 'views/page.twig', $context );
