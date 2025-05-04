4c0d9e5a97d4f08f52d9aeb556fcb7649dced270
Julian Peroncini <julianperoncini@gmail.com>
2025-05-04 18:13:16 -0300

    fixes

--- wp-content/themes/outten-and-golden/page.php
+++ wp-content/themes/outten-and-golden/page.php
@@ -16,4 +16,17 @@ $context = Timber::context();
 $context['post'] = Timber::get_post();
 $context['content_builder'] = get_fields($context['post']->ID)['content_builder'] ?? null;
 
-Timber::render( 'views/page.twig', $context );
+$post_types = ['cases', 'newsroom', 'client-stories', 'team-member', 'issues'];
+
+foreach ($post_types as $post_type) {
+    $context_key = str_replace('-', '_', $post_type);
+    
+    $args = [
+        'post_type' => $post_type,
+        'posts_per_page' => -1,
+    ];
+    
+    $context[$context_key] = Timber::get_posts($args);
+}
+
+Timber::render('views/page.twig', $context);
